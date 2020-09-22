02417e8966dbf0d0f5fd8f674091fcf42a10a403
zoltán balogh <zolikaaa1@gmail.com>
2020-09-22 09:13:57 +0200

    még 1 sor

---
+++ 09.22/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor =ConsoleColor.Green;
+
+            Console.WriteLine("Balogh Zoltán");
+            Console.ReadLine();
+
+        }
+    }
+}
